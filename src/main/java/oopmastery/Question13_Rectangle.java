b0043e2e3a391d7632f1458c9928bd88491f0383
benitmatumona <polviedivine@gmail.com>
2026-06-14 23:54:36 +0200

    updated files

---
+++ src/main/java/oopmastery/Question13_Rectangle.java
@@ -0,0 +1,24 @@
+package oopmastery;
+
+public class Question13_Rectangle
+        extends Question13_Shape {
+
+    private double width;
+    private double height;
+
+    public Question13_Rectangle(
+            double width,
+            double height
+    ) {
+        // TODO:
+        // Store width and height
+    }
+
+    @Override
+    public double area() {
+        // TODO:
+        // Return:
+        // width * height
+        return 0;
+    }
+}
