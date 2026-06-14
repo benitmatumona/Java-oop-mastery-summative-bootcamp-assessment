b4e32ab71c203fc609e6720edcbcb260998dc6ed
benitmatumona <polviedivine@gmail.com>
2026-06-14 23:32:59 +0200

    updated files

---
+++ src/main/java/oopmastery/Question11_EmailNotification.java
@@ -0,0 +1,13 @@
+package oopmastery;
+
+public class Question11_EmailNotification
+        implements Question11_NotificationService {
+
+    @Override
+    public String send(String message) {
+        // TODO:
+        // Return:
+        // "Email sent: " + message
+        return "";
+    }
+}
